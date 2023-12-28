ff681bf32899017b15c611fe470bd8becf6aa956
binarycraft007 <elliot.huang.signed@gmail.com>
2023-12-28 11:02:22 +0800

    optimize init process

--- src/main.zig
+++ src/main.zig
@@ -28,13 +28,15 @@ fn waitForDriverReady() !void {
     // Function times out after 10 seconds
     const count = (POLL_DRIVER_MAX_TIME_MS * 1000) / POLL_DRIVER_DURATION_US;
     for (0..count) |_| {
-        const file_maybe = std.fs.openDirAbsolute("/sys/class/net/wlan0", .{});
-        if (file_maybe) |file| {
-            file.close();
-            return;
-        } else |_| {
+        var buf: [128]u8 = undefined;
+        var file = std.fs.openFileAbsolute("/sys/class/net/wlan0/operstate", .{}) catch {
             std.time.sleep(std.time.ns_per_us * POLL_DRIVER_DURATION_US);
-        }
+            continue;
+        };
+        defer file.close();
+        const data = try file.reader().readUntilDelimiter(&buf, '\n');
+        if (mem.eql(u8, data, "UP")) return;
+        return;
     }
     return error.TimeOut;
 }
@@ -101,11 +103,45 @@ fn setCountryCodeBcm(state: nl80211.State, country_code: [*c]const u8) !void {
         nl80211.NL80211_ATTR_VENDOR_SUBCMD,
         bcm.WIFI_SUBCMD_SET_COUNTRY_CODE,
     );
-    msg.put_u32(nl80211.NL80211_ATTR_IFINDEX, 46);
+    msg.put_u32(nl80211.NL80211_ATTR_IFINDEX, try getMinWifiIndex());
     const data = msg.nest_start(nl80211.NL80211_ATTR_VENDOR_DATA);
     msg.put_string(bcm.ANDR_WIFI_ATTRIBUTE_COUNTRY, country_code);
     msg.nest_end(data);
     try msg.send();
+}
+
+fn getMinWifiIndex() !u32 {
+    var min_idx: u32 = 0xFFFFFFFF;
+    var dir = try std.fs.openDirAbsolute("/sys/class/net", .{ .iterate = true });
+    defer dir.close();
+    var iter = dir.iterate();
+    while (try iter.next()) |entry| {
+        if (isWifiInterface(entry.name)) {
+            var buf: [128]u8 = undefined;
+            const path = try std.fmt.bufPrint(&buf, "{s}/ifindex", .{entry.name});
+            var file = try dir.openFile(path, .{});
+            defer file.close();
+            const data = try file.reader().readUntilDelimiter(&buf, '\n');
+            const idx = try std.fmt.parseInt(u32, data, 10);
+            min_idx = @min(idx, min_idx);
+        }
+    }
+    return min_idx;
+}
+
+fn isWifiInterface(name: []const u8) bool {
+    const prefixes = [_][]const u8{
+        "wlan",
+        "swlan",
+        "p2p",
+        "aware",
+    };
+    for (prefixes) |prefix| {
+        if (mem.startsWith(u8, name, prefix)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 test "simple test" {}
